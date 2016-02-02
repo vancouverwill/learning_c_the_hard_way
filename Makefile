db95e90ab681eade252de2e156014d82d0738b56
Will Melbourne <willmelbourne@gmail.com>
2016-02-02 18:14:01 +0000

    intial commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-Wall -g
+
+all:
+	./ex1.c ./ex3.c
+
+clean:
+	rm -f ex1 ex3
---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello World");
+	puts("Hello World 2");
+
+	puts("Hello World 3");
+	
+	return 0;
+}
