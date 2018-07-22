e4605c78c3a97442b778bc5addbf1791676bad69
William Melbourne <willmelbourne@gmail.com>
2018-07-22 16:33:54 -0700

    split up ex29 into makefile and simple build script versions

--- ex29/ex29-basic/buildScript.sh
+++ ex29/ex29-basic/buildScript.sh
@@ -1,6 +1,9 @@
 rm *.o *.so ex29
+# compile the lib file and make the .so
 cc -c libex29.c -o libex29.o
 cc -shared -o libex29.so libex29.o
+
+# make the loader program
 cc -Wall -g -DNDEBUG ex29.c -ldl -o ex29
 ./ex29 ./libex29.so lowercase  "hello THERE"
 ./ex29 ./libex29.so uppercase  "hello THERE"
